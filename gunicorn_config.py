9f247afc2fbc49c4378c6490c4f3c5d770a23a86
Juan Park <111858577+jpjp92@users.noreply.github.com>
2024-12-26 00:45:04 +0900

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,11 @@
+# gunicorn_config.py
+workers = 2
+threads = 4
+timeout = 600
+max_requests = 1000
+max_requests_jitter = 50
+worker_class = 'gthread'
+worker_tmp_dir = '/dev/shm'
+
+# Memory limits
+worker_max_memory_per_child = 200000  # 200MB
